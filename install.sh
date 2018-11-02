907f4805eff84808b177860c5fbfe596ff094da7
Callum <gavzilla+github@gmail.com>
2018-11-02 13:01:27 +0900

    Add vim-sensible

--- install.sh
+++ install.sh
@@ -1,8 +1,12 @@
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
+git clone https://github.com/tpope/vim-sensible.git ~/.vim/bundle/sensible
+
 git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
 
-git clone https://github.com/kien/ctrlp.vim.git bundle/ctrlp.vim
+git clone https://github.com/kien/ctrlp.vim.git ~/.vim/bundle/ctrlp.vim
 
 cp ./.gitconfig ~/
+cp ./.vimrc ~/
+cp ./.tmux.conf ~/
