e5781d749ea593a351e5a6f2c7d9b232107774b4
Callum <mullacy@gmail.com>
2018-11-02 12:37:43 +0900

    Better first-time install

--- install.sh
+++ install.sh
@@ -4,3 +4,5 @@ curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
 
 git clone https://github.com/kien/ctrlp.vim.git bundle/ctrlp.vim
+
+cp ./.gitconfig ~/
