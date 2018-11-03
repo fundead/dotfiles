aea1491afdb54959d175ca4a5176b2ca93927f08
Callum <gavzilla+github@gmail.com>
2018-11-03 09:47:21 +0900

    Add airline

--- install.sh
+++ install.sh
@@ -7,6 +7,10 @@ git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
 
 git clone https://github.com/kien/ctrlp.vim.git ~/.vim/bundle/ctrlp.vim
 
+git clone https://github.com/vimwiki/vimwiki.git ~/.vim/bundle/vimwiki
+
+git clone https://github.com/vim-airline/vim-airline ~/.vim/bundle/vim-airline
+
 cp ./.gitconfig ~/
 cp ./.vimrc ~/
 cp ./.tmux.conf ~/
